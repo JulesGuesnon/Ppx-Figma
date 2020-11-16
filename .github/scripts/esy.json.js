943848df61e59d4b77e237189bcd0f9ef677967b
Jules Guesnon <jules.guesnon@hetic.net>
2020-11-16 14:22:15 +0100

    :green_hear: Added resolution to ocaml to fix build error on mac

--- .github/scripts/esy.json.js
+++ .github/scripts/esy.json.js
@@ -1,12 +1,25 @@
 const fs = require('fs')
+const { platform } = require('os')
 const path = require('path')
 
 const mainEsyJson = require('../../esy.json')
 
 if (!mainEsyJson.resolutions) mainEsyJson.resolutions = {}
 
-mainEsyJson.resolutions['@opam/conf-libssl'] =
-    'esy-packages/esy-openssl#4476291'
+if (platform == 'win32')
+    mainEsyJson.resolutions['@opam/conf-libssl'] =
+        'esy-packages/esy-openssl#4476291'
+
+if (platform == 'darwin')
+    mainEsyJson.resolutions['ocaml'] = {
+        source: 'esy-ocaml/ocaml#1805622dc40c1c569a1720078b5707eeba32b07d',
+        override: {
+            build: [
+                './esy-configure -no-cfi -cc "gcc -Wno-implicit-function-declaration" -prefix $cur__install',
+                './esy-build',
+            ],
+        },
+    }
 
 const esyJson = JSON.stringify(mainEsyJson, null, 2)
 
