e075a27eb999607f08cd3d3386f6aa343f020fec
Jules Guesnon <jules.guesnon@hetic.net>
2020-11-16 13:50:55 +0100

    :green_heart: Fixed esy script

--- .github/scripts/esy.json.js
+++ .github/scripts/esy.json.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const mainEsyJson = require('../../esy.json')
 
+if (!mainEsyJson.resolutions) mainEsyJson.resolutions = {}
+
 mainEsyJson.resolutions['@opam/conf-libssl'] =
     'esy-packages/esy-openssl#4476291'
 
